621da72b65250d8f792ef1caa8282c8064103441
kevin daliri <kevindaliri@kevins-MacBook-Pro.local>
2024-11-22 21:29:56 -0800

    Fix default exports in Database files

---
+++ src/Kanbas/Database/grades.js
@@ -0,0 +1,5 @@
+export default [
+    { _id: "1", student: "121", assignment: "A101", grade: 90 },
+    { _id: "2", student: "122", assignment: "A102", grade: 85 },
+    { _id: "3", student: "123", assignment: "A103", grade: 95 },
+  ];
